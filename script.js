e46aed4975ef6420da49b74dc417445bb59e3941
Don Svederus <svederus@gmail.com>
2022-06-09 00:23:36 -0600

    0.74.1 js menu functions

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let size = 20;
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearel = document.getElementById('clear');
+
+let size = 10;
 let isPressed = false;
 let color = 'black';
 let x;
@@ -52,3 +58,31 @@ function drawLine(x1, y1, x2, y2) {
     ctx.lineWidth = size * 2;
     ctx.stroke();
 }
+
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5;
+
+    if(size > 50) {
+        size = 50;
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5;
+
+    if(size < 5) {
+        size = 5;
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearel.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
